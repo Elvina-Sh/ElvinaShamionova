28373306da4ea0f7df58551f1ecd409c6fc99d64
Elvina-Sh <elvinashamionova@gmail.com>
2021-08-10 23:12:35 +0300

    BaseTest class is updated

--- hw1/src/test/java/com/epam/tc/hw1/BaseTest.java
+++ hw1/src/test/java/com/epam/tc/hw1/BaseTest.java
@@ -1,14 +1,20 @@
 package com.epam.tc.hw1;
 
 import com.epam.tat.module4.Calculator;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-    static Calculator calculator;
+    protected Calculator calculator;
 
-    @BeforeTest(alwaysRun = true)
-    public void beforeTest() {
+    @BeforeMethod(alwaysRun = true)
+    public void beforeMethod() {
         calculator = new Calculator();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod() {
+        calculator = null;
     }
 
 }
